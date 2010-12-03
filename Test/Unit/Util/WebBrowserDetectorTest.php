eef32e9df04f02ad824fc632d82a20a5c4018e98
Dejan Spasic <dejan.spasic@twt.de>
2010-12-03 17:10:41 +0100

    add test envorinment

---
+++ Test/Unit/Util/WebBrowserDetectorTest.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . "/../../../Fam/Util/WebBrowserDetector.php";
+
+class Fam_Util_WebBrowserDetector extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+}
