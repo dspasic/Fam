2c476d6ce7e1e9047ae16d8b9798a96389c5574a
Dejan Spasic <spasic.dejan@yahoo.de>
2010-12-03 21:17:29 +0100

    append Test to class name

--- Test/Unit/Util/WebBrowserDetectorTest.php
+++ Test/Unit/Util/WebBrowserDetectorTest.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . "/../../../Fam/Util/WebBrowserDetector.php";
 
-class Fam_Util_WebBrowserDetector extends PHPUnit_Framework_TestCase
+class Fam_Util_WebBrowserDetectorTest extends PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
