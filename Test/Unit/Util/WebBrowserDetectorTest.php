370f0bca1ee74298d764ea4a4f0616581bd9e44f
Dejan Spasic <spasic.dejan@yahoo.de>
2010-12-05 19:03:23 +0100

    add set up and tear down to backup and restore some global vars

--- Test/Unit/Util/WebBrowserDetectorTest.php
+++ Test/Unit/Util/WebBrowserDetectorTest.php
@@ -4,7 +4,15 @@ require_once __DIR__ . "/../../../Fam/Util/WebBrowserDetector.php";
 
 class Fam_Util_WebBrowserDetectorTest extends PHPUnit_Framework_TestCase
 {
+    private $originHttpUserAgent = null;
+
     protected function setUp()
     {
+        $this->originHttpUserAgent = isset($_ENV["HTTP_USER_AGENT"]) ? $_ENV["HTTP_USER_AGENT"] : null;
+    }
+
+    protected function tearDown()
+    {
+        $_ENV["HTTP_USER_AGENT"] = $this->originHttpUserAgent;
     }
 }
