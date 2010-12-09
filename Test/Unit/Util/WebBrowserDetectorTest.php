a5ba0bf2868c7a68d9cc4c1b6922cc6b0e0d40d9
Dejan Spasic <dejan.spasic@twt.de>
2010-12-09 15:19:53 +0100

    add tests for method webClientVersion

--- Test/Unit/Util/WebBrowserDetectorTest.php
+++ Test/Unit/Util/WebBrowserDetectorTest.php
@@ -31,11 +31,30 @@ class Fam_Util_WebBrowserDetectorTest extends PHPUnit_Framework_TestCase
      * @test
      * @dataProvider userAgentWebClientDataProvider
      */
-    public function detectWebClient($userAgend, $expectedWebClient)
+    public function detectWebClient($userAgent, $expectedWebClient)
     {
-        putenv("HTTP_USER_AGENT=" . $userAgend);
+        putenv("HTTP_USER_AGENT=" . $userAgent);
         $this->assertEquals($expectedWebClient, \Fam\Util\WebBrowserDetector::webClient());
     }
+    
+    /**
+     * @test
+     */
+    public function webClientVersion_withUndefinedUserAgent()
+    {
+        putenv("HTTP_USER_AGENT=Lynx/2.8.4rel.1 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/0.9.6c");
+        $this->assertEquals(\Fam\Util\WebBrowserDetector::WEBCLIENT_UNDEFINED, \Fam\Util\WebBrowserDetector::webClientVersion());
+    }
+
+    /**
+     * @test
+     */
+    public function webClientVersion_withMSIE_60()
+    {
+        putenv("HTTP_USER_AGENT=Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0");
+        $this->assertEquals(6.0, \Fam\Util\WebBrowserDetector::webClientVersion());
+    }
+    
 
     public function userAgentOsDataProvider()
     {
