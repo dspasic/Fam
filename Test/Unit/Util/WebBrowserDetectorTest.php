d9554fb9bf4aadaab163e68dcd933a3aa837fc5c
Dejan Spasic <spasic.dejan@yahoo.de>
2010-12-09 10:08:20 +0100

    first tests for method detectOs

--- Test/Unit/Util/WebBrowserDetectorTest.php
+++ Test/Unit/Util/WebBrowserDetectorTest.php
@@ -8,11 +8,47 @@ class Fam_Util_WebBrowserDetectorTest extends PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->originHttpUserAgent = isset($_ENV["HTTP_USER_AGENT"]) ? $_ENV["HTTP_USER_AGENT"] : null;
+        $this->originHttpUserAgent = false !== getenv("HTTP_USER_AGENT") ? getenv("HTTP_USER_AGENT") : null;
     }
 
     protected function tearDown()
     {
-        $_ENV["HTTP_USER_AGENT"] = $this->originHttpUserAgent;
+        \Fam\Util\WebBrowserDetector::restoreInstance();
+        putenv("HTTP_USER_AGENT=" . $this->originHttpUserAgent);
+    }
+    
+    /**
+     * @test
+     * @dataProvider userAgentOsDataProvider
+     */
+    public function detectOs($userAgent, $expected)
+    {
+        putenv("HTTP_USER_AGENT=" . $userAgent);
+        $this->assertEquals($expected, \Fam\Util\WebBrowserDetector::os());
+    }
+    
+    public function userAgentOsDataProvider()
+    {
+        return array(
+            array(
+            	"Lynx/2.8.4rel.1 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/0.9.6c", 
+                \Fam\Util\WebBrowserDetector::OS_UNDEFINED,   
+            ),
+            
+            array(
+                "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)",
+                \Fam\Util\WebBrowserDetector::OS_WIN,
+            ),
+            
+            array(
+                "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/125.2 (KHTML, like Gecko) Safari/125.8",
+                \Fam\Util\WebBrowserDetector::OS_MAC,
+            ),
+            
+            array(
+                "Mozilla/5.0 (compatible; Konqueror/3.2; Linux 2.6.2) (KHTML, like Gecko)",
+                \Fam\Util\WebBrowserDetector::OS_UNIX,
+            ),
+        );
     }
 }
