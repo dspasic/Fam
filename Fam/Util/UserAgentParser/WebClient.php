4942eec3c94552f92e91b7741e134073ff206b8a
Dejan Spasic <spasic.dejan@yahoo.de>
2011-01-10 18:07:33 +0100

    add interface for webclient

---
+++ Fam/Util/UserAgentParser/WebClient.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * (c) 2008 Dejan Spasic <sapsic.dejan@yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    Util
+ * @subpackage UserAgentParser
+ * @author     Dejan Spasic <spasic.dejan@yahoo.de>
+ * @version    GIT: $Id:$
+ */
+
+declare(encoding="UTF-8");
+
+namespace Fam\Util\UserAgentParser;
+
+/**
+ * Reproduce a web client
+ *
+ * @package    Util
+ * @subpackage UserAgentParser
+ * @author     Dejan Spasic <spasic.dejan@yahoo.de>
+ * @version    @@PACKAGE_VERSION@@
+ */
+interface WebClient
+{
+    /**
+     * @param string $userAgent
+     *
+     * @return boolean
+     */
+    public function match($userAgent);
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @param string|Fam\Util\UserAgentParser\WebClient $webClient
+     *
+     * @return boolean
+     */
+    public function isNameEquals($webClient);
+
+    /**
+     * @return string
+     */
+    public function getVersion();
+
+    /**
+     * @param string $version
+     *
+     * @return boolean
+     */
+    public function isVersionEquals($version);
+
+    /**
+     * @param string $lowerVersion
+     * @param string $greateVersion
+     *
+     * @return boolean
+     */
+    public function isVersionBetween($lowerVersion, $greaterVersion);
+}
