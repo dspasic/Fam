67384060608e6ac0d4514aa50493986f76323b9c
Dejan Spasic <spasic.dejan@yahoo.de>
2011-01-17 22:59:04 +0100

    add opera and safari

---
+++ Test/Unit/Util/UserAgentParser/OperaTest.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once __DIR__ . "/../../../../Fam/Util/UserAgentParser/Opera.php";
+
+class Fam_Util_UserAgentParser_OperaTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Fam\Util\UserAgentParser\Firefox
+     */
+    private $subject;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->subject = new \Fam\Util\UserAgentParser\Opera();
+    }
+
+    /**
+     * @test
+     */
+    public function match_WithValidValue()
+    {
+        $this->assertTrue($this->subject->match("Opera/9.99 (Windows NT 5.1; U; pl) Presto/9.9.9"));
+    }
+
+    /**
+     * @test
+     */
+    public function match_WithInvalidValue()
+    {
+        $this->assertFalse($this->subject->match("Lynx/2.8.4rel.1 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/0.9.6c"));
+    }
+
+    /**
+     * @test
+     */
+    public function getName_IsEqualsFirefox()
+    {
+        $this->assertEquals('opera', $this->subject->getName());
+    }
+}
---
+++ Test/Unit/Util/UserAgentParser/SafariTest.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . "/../../../../Fam/Util/UserAgentParser/Safari.php";
+
+class Fam_Util_UserAgentParser_SafariTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Fam\Util\UserAgentParser\Firefox
+     */
+    private $subject;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->subject = new \Fam\Util\UserAgentParser\Safari();
+    }
+
+    /**
+     * @test
+     */
+    public function match_WithValidValue()
+    {
+        $this->assertTrue($this->subject->match(
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-HK) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5"
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function match_WithInvalidValue()
+    {
+        $this->assertFalse($this->subject->match("Lynx/2.8.4rel.1 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/0.9.6c"));
+    }
+
+    /**
+     * @test
+     */
+    public function getName_IsEqualsFirefox()
+    {
+        $this->assertEquals('safari', $this->subject->getName());
+    }
+}
